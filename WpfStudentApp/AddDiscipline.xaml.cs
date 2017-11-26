97296a8b4fa72dea58363daf465a45307a4ae238
yurchyk21 <fanya21@gmail.com>
2017-11-26 11:37:42 +0200

    + Del Discipline, +Changed Window with Disciplines.

--- WpfStudentApp/AddDiscipline.xaml.cs
+++ WpfStudentApp/AddDiscipline.xaml.cs
@@ -20,11 +20,16 @@ namespace WpfStudentApp
     /// </summary>
     public partial class AddDiscipline : Window
     {
+        bool isSaved = true;
         StudentService stud;
         public AddDiscipline(StudentService stud)
         {
             InitializeComponent();
             this.stud = stud;
+            foreach (string item in stud.GetDisciplines)
+            {
+                ComboDiscipName.Items.Add(item);
+            }
         }
 
         private void add_ocin_Click(object sender, RoutedEventArgs e)
@@ -40,9 +45,58 @@ namespace WpfStudentApp
             else
             {
                 stud.AddDiscipline(DiscipName.Text);
-                stud.SaveDisciplines();
+                ComboDiscipName.Items.Add(DiscipName.Text);
+                SaveDisciplines.IsEnabled = true;
+                DiscipName.Clear();
+                isSaved = false;
+                MessageBox.Show("Дисципліну додано", "Info", MessageBoxButton.OK);
             }
 
+        }
+
+        private void DelDisc_Click(object sender, RoutedEventArgs e)
+        {
+            if (ComboDiscipName.SelectedIndex != -1)
+            {
+                stud.GetDisciplines.Remove(ComboDiscipName.Text);
+                ComboDiscipName.Items.RemoveAt(ComboDiscipName.SelectedIndex);
+                SaveDisciplines.IsEnabled = true;
+                isSaved = false;
+                MessageBox.Show("Дисципліну видалено", "Info", MessageBoxButton.OK);
+            }
+            else if (ComboDiscipName.Items.Count == 0)
+            {
+                MessageBox.Show("Немає дисциплін у базі", "Error", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("Оберіть дисципліну", "Error", MessageBoxButton.OK);
+            }
+        }
+
+        private void SaveDisciplines_Click(object sender, RoutedEventArgs e)
+        {
+            stud.SaveDisciplines();
+            isSaved = true;
+            MessageBox.Show("Зміни збережено", "Info", MessageBoxButton.OK);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult dialog;
+            if (!isSaved)
+            {
+                dialog = MessageBox.Show("Не збережені зміни після закриття програми буде втрачено. Зберегти зміни \"Так\". Продовжити без збереження \"Ні\"", "Warning", MessageBoxButton.YesNoCancel);
+                if (dialog == MessageBoxResult.Cancel)
+                {
+                    e.Cancel = true; 
+                }
+                else if (dialog == MessageBoxResult.Yes)
+                {
+                    stud.SaveDisciplines();
+                    MessageBox.Show("Зміни збережено", "Info", MessageBoxButton.OK);
+                }
+            }
         }
     }
 }
